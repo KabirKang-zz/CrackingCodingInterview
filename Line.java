17776941fc644d7e01df0cbd1a0ef6d80ca3e998
Kabir <kangkabir@gmail.com>
2015-01-07 09:49:18 -0800

    Question 7.3 Answered

---
+++ Line.java
@@ -0,0 +1,20 @@
+public class Line{
+    static double epsilon = 0.000001;
+    public double slope;
+    public double yint;
+    public Line(double s, double y){
+        slope = s;
+        yint = y;
+    }
+    public boolean intersect(Line line2){
+        return Math.abs(slope - line2.slope)>epsilon || 
+	    Math.abs(yint-line2.yint)<epsilon;
+    }
+    public static void main(String [] args){
+	System.out.println("Hello");
+	Line line1,line2;
+	line1 = new Line(2,2);
+	line2 = new Line(2,3);
+	System.out.println(line1.intersect(line2));
+    }
+}
