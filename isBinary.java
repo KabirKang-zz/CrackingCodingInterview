da6f34f83ee30382a0d991ebce41c3405d9ea4c0
Kabir <kangkabir@gmail.com>
2015-01-09 17:35:22 -0800

    14.1 answered

--- isBinary.java
+++ isBinary.java
@@ -6,3 +6,16 @@ The other methodology is to use the definition of BST that says left<=curr<right
 We must ensure that ALL left nodes are less than or equal to the current node, 
 which must be less than ALL right nodes.
 */
+
+public class isBinary{
+    public class TreeNode{
+	public int data;
+	public TreeNode left;
+	public TreeNode right;
+	public TreeNode parent;
+	private int size = 0;
+    }
+    public static void main(String[] args)
+    {
+    }
+}
